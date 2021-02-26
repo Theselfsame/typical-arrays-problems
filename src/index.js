7df0711cd7789a96451bde6462a034cc2acee0e3
Sergey Matiushkin <74297848+Theselfsame@users.noreply.github.com>
2021-02-26 19:37:50 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+	    let arr = array.sort(function(a, b) {
+		return(a - b);
+	});
+	return(arr[0]);
 }
+  
 
 exports.max = function max (array) {
-  return 0;
+	let arr = array.sort(function(a, b){
+		return(b - a);
+	});
+  return(arr[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	let arr = array;
+	let len = arr.length;
+	let sum = 0;
+	for(let i = 0; i < len;i++) {
+		sum += arr[i];
+	}
+  return(sum / len);
 }
