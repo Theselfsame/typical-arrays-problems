5ad53284aad9a8a5ab796140b59de8ac2f82d889
Sergey Matiushkin <74297848+Theselfsame@users.noreply.github.com>
2021-03-07 19:40:44 +0300

    feat: add final code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-
 exports.min = function min (array) {
-	    let arr = array.sort(function(a, b) {
-		return(a - b);
+	if (!Array.isArray(array) || array.length == 0) {
+		return 0;
+	} else {
+    let arr = array.sort(function(a, b) {
+	return(a - b);
 	});
 	return(arr[0]);
+    }
 }
   
 
 exports.max = function max (array) {
+	if (!Array.isArray(array) || array.length == 0) {
+		return 0;
+	} else {
 	let arr = array.sort(function(a, b){
 		return(b - a);
 	});
   return(arr[0]);
+    }
 }
 
 exports.avg = function avg (array) {
+	if (!Array.isArray(array) || array.length == 0) {
+		return 0;
+	} else {
 	let arr = array;
 	let len = arr.length;
 	let sum = 0;
@@ -22,4 +32,5 @@ exports.avg = function avg (array) {
 		sum += arr[i];
 	}
   return(sum / len);
+    }
 }
